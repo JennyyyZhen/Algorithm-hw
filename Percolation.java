316cf33cb788a6d40b335aa986bcf06814f01bc3
JennyyyZhen <45784247+JennyyyZhen@users.noreply.github.com>
2018-12-14 22:44:15 -0800

    Update Percolation.java

--- Percolation.java
+++ Percolation.java
@@ -1,88 +1,129 @@
 package coursera;
-
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-public class PercolationStats {
 
-	private double[] thresholds;
-	private int trials;
-	private int size;
-	private final double mean;
-	private final double stddev;
-	private final double confL;
-	private final double confH;
+public class Percolation {
 
-	// perform T independent computational experiments on an N-by-N grid
-	public PercolationStats(int N, int T) {
-		if (N <= 0)
-			throw new java.lang.IllegalArgumentException("N is out of bounds");
-		if (T <= 0)
-			throw new java.lang.IllegalArgumentException("T is out of bounds");
+	  private int size;
+	  private int width;
+	  private int[] status;
+	  private WeightedQuickUnionUF uf;
+	  private boolean percolate;
+	  private int openSites;
 
-		this.size = N;
-		this.trials = T;
-		this.thresholds = new double[trials];
+	  // create N-by-N grid, with all sites blocked
+	  public Percolation(int N) {
+		  if(N<=0) {
+			  throw new IllegalArgumentException();
+		  }
+	    size = N*N;
+	    width = N;
+	    status = new int[size];
+	    uf = new WeightedQuickUnionUF(size);
+	    openSites=0;
+	    percolate=false;
+	    for(int i=0;i<size;i++) {
+	    	if(i>=0 && i<width) {
+	    		byte b=010;//blocked,connected to top,not connected to bottom
+	    		status[i]=status[i]|2;
+	    	}
+	    	if(i>=size-width && i<size) {
+	    		status[i]=status[i]|1;
+	    	}
+	    }
+	  }
 
-		for (int i = 0; i < T; i++) {
-			thresholds[i] = findThreshold();
-		}
-		this.mean=StdStats.mean(thresholds);
-		if (trials == 1)
-			this.stddev=Double.NaN;
-		else
-			this.stddev=StdStats.stddev(thresholds);
-		this.confL=mean - 1.96 * stddev / Math.sqrt(trials);
-		this.confH=mean + 1.96 * stddev / Math.sqrt(trials);
-	}
+	  // open site (row i, column j) if it is not already
+	  public void open(int i, int j) {
+	    checkBounds(i, j);
+	    if(isOpen(i,j)) return;
+	    int num = ijTo1D(i, j);
+	    status[num]= status[num]|4;
+	    connectToOpenNeighbors(i, j);
+	    int root=uf.find(num);
+	    status[root]=status[num]|status[root];
+	   // System.out.println(root+ " "+status[root]);
+	    if(status[root]==7)
+	    	percolate=true;
+	    openSites++;
+	  }
 
-	// sample mean of percolation threshold
-	public double mean() {
-		return mean;
-	}
+	  // is site (row i, column j) open?
+	  public boolean isOpen(int i, int j) {
+	    checkBounds(i, j);
+	    int num = ijTo1D(i, j);
+	    return (status[num] >=4);
+	  }
 
-	// sample standard deviation of percolation threshold
-	public double stddev() {
-		
-		return stddev;
-	}
+	  // is site (row i, column j) full?
+	  public boolean isFull(int i, int j) {
+	    checkBounds(i, j);
+	    return status[uf.find(ijTo1D(i,j))]>=6;
+	  }
 
-	// returns lower bound of the 95% confidence interval
-	public double confidenceLo() {
-		
-		return confL;
-	}
+	  // does the system percolate?
+	  public boolean percolates() {
+	    return percolate;
+	  }
+	  public int numberOfOpenSites() {
+		  return openSites;
+	  }
 
-	// returns upper bound of the 95% confidence interval
-	public double confidenceHi() {
-		
-		return confH;
-	}
+	  private void connectToOpenNeighbors(int i, int j) {
+	    int index = ijTo1D(i, j);
+	    if (j < width) attemptUnion(i, j+1, index);
+	    if (j > 1) attemptUnion(i, j-1, index);
 
-	private double findThreshold() {
-		Percolation perc = new Percolation(size);
-		int i, j;
-		int count = 0;
-		while (!perc.percolates()) {
-			do {
-				i = StdRandom.uniform(size) + 1;
-				j = StdRandom.uniform(size) + 1;
-			} while (perc.isOpen(i, j));
-			count++;
-			perc.open(i, j);
-		}
-		return count / (Math.pow(size, 2));
-	}
+	    if (i < width) {
+	      attemptUnion(i+1, j, index);
+	    }
+	    /*else {
+	      uf.union(index, size+1);
+	    }
+	    */
+	    if (i > 1) {
+	      attemptUnion(i-1, j, index);
+	    }/* else {
+	      uf.union(index, size);
+	    }*/
+	  }
 
-	// test client, described below
-	public static void main(String[] args) {
-		int N = Integer.parseInt(args[0]);
-		int T = Integer.parseInt(args[1]);
-		PercolationStats stats = new PercolationStats(N, T);
-		StdOut.printf("mean = %f\n", stats.mean());
-		StdOut.printf("stddev = %f\n", stats.stddev());
-		StdOut.printf("95%% confidence interval = %f, %f\n", stats.confidenceLo(), stats.confidenceHi());
-		stats.confidenceLo();
-	}
-}
+	  private void attemptUnion(int i, int j, int index) {
+	    if (isOpen(i, j)) {
+	     int num = ijTo1D(i, j);
+	     status[index]=status[uf.find(num)]|status[index];
+	      uf.union(num, index);
+	      int root=uf.find(index);
+	      status[root]=status[index];
+	    }
+	  }
+
+	  // converts index in ij notation to a single 1D index (zero-indexed)
+	  private int ijTo1D(int i, int j) {
+	    return ((i*width - width) + j) - 1;
+	  }
+
+	  private void checkBounds(int i, int j) {
+	    if (i <= 0 || i > width) {
+	      throw new java.lang.IllegalArgumentException("row index i out of bounds");
+	    }
+	    if (j <= 0 || j > width) {
+	      throw new java.lang.IllegalArgumentException("row index i out of bounds");
+	    }
+	  }
+
+	  public static void main(String[] args) {
+	    Percolation perc = new Percolation(3);
+	    perc.open(1, 2);
+	    perc.open(2, 2);
+	    perc.open(2, 3);
+	    perc.open(3, 3);
+	    boolean c = perc.isFull(1, 1);
+	    //boolean c1 = perc.uf.connected(perc.ijTo1D(1, 1), perc.ijTo1D(2, 1));
+	    //boolean c2 = perc.percolates();
+	    StdOut.println(perc.numberOfOpenSites());
+	    //StdOut.println(c1);
+	    //StdOut.println(c2);
+	  }
+
